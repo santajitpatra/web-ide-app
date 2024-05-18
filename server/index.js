e65be700bdf755e228898f774cd7062798846eb0
Santajit Patra <121734312+santajitpatra@users.noreply.github.com>
2024-05-19 01:59:03 +0530

    feat(server): Enable real-time terminal interaction via Socket.io
    feat(server): Enable real-time terminal interaction via Socket.io
    
    * Sets up a Node.js server with real-time capabilities for terminal access.
    * Integrates Socket.io to stream terminal output and handle user input.
    * Utilizes `pty` to create a pseudo-terminal based on user's platform (bash/powershell).
    * Allows users to send commands to the terminal through the "terminal:write" event.
    * Provides a foundation for building collaborative or remote terminal applications.

--- server/index.js
+++ server/index.js
@@ -1,6 +1,18 @@
 const express = require("express");
-const { http } = require("http");
+const http = require("node:http");
+const os = require("os");
 const { Server: SocketServer } = require("socket.io");
+const pty = require("node-pty");
+
+const shell = os.platform() === "win32" ? "powershell.exe" : "bash";
+
+const ptyProcess = pty.spawn(shell, [], {
+  name: "xterm-color",
+  cols: 80,
+  rows: 30,
+  cwd: process.env.INIT_CWD,
+  env: process.env,
+});
 
 const app = express();
 const server = http.createServer(app);
@@ -8,16 +20,25 @@ const io = new SocketServer({
   cors: "*",
 });
 
-io.attach(server)
+io.attach(server);
 
 // app.get("/", (req, res) => {
 //   res.send("<h1>Hello world</h1>");
 // });
 
-io.on('connection', (socket) => {
-  console.log('a user connected', socket.id);
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
+ptyProcess.onData((data) => {
+  io.emit("terminal:data", data);
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected", socket.id);
+
+  socket.on("terminal:write", (data) => {
+    ptyProcess.write(data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
   });
 });
 
