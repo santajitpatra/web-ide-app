1774bde70fddd7adfc0dab01bbf8b0af1f90c017
Santajit Patra <121734312+santajitpatra@users.noreply.github.com>
2024-05-21 17:59:31 +0530

    feat(app): Integrate real-time terminal and enhance UI layout
    feat(app): Integrate real-time terminal and enhance UI layout
    
    * Integrates `Terminal` component into the main `App` component for user interaction.
    * Defines basic UI structure for the terminal within a designated container.
    * Introduces initial styles for the "playground-container," "editor-container," and "terminal-container."
    * Lays the groundwork for building a comprehensive real-time terminal user interface.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,18 @@
-import Terminal from "./components/terminal"
+import Terminal from "./components/terminal";
+import "./App.css";
 
 function App() {
   return (
-    <div>
-      <div>
-        <Terminal/>
+    <div className="playground-container">
+      <div className="editor-container">
+        <div className="files"></div>
+        <div className="editor"></div>
+      </div>
+      <div className="terminal-container">
+        <Terminal />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/components/terminal.jsx
+++ client/src/components/terminal.jsx
@@ -1,29 +1,29 @@
 import { Terminal as XTerminal } from "@xterm/xterm";
 import { useEffect, useRef } from "react";
 import "@xterm/xterm/css/xterm.css";
+import socket from "../socket";
 
 const Terminal = () => {
+  const terminalRef = useRef();
+  const isRendered = useRef(false);
 
-    const terminalRef = useRef();
-    const isRendered = useRef(false);
+  useEffect(() => {
+    if (isRendered.current) return;
+    isRendered.current = true;
 
-    useEffect(() => {
-      if (isRendered.current) return;
-      isRendered.current = true;
-      
-        const term = new XTerminal({
-            rows: 30,
-        });
-        term.open(terminalRef.current);
+    const term = new XTerminal({
+      rows: 30,
+    });
+    term.open(terminalRef.current);
 
-        term.onData((data) => {
-            console.log(data);
-        });  
-    }, [])
-  return (
-    <div ref={terminalRef} id="terminal"/>
-  )
-}
+    term.onData((data) => {
+      socket.emit("terminal:write", data);
+    });
+    socket.on("terminal:data", (data) => {
+      term.write(data);
+    });
+  }, []);
+  return <div ref={terminalRef} id="terminal" />;
+};
 
-export default Terminal
-
+export default Terminal;
